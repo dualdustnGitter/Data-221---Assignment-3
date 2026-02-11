8d40b16a169ad8c87106510db8c62a80e098c7c6
DualDustin <dualdustin@gmail.com>
2026-02-11 12:51:40 -0700

    Finish question 1 with comments and begin with question 2 to continue on assignment

--- Assignment 3 - Question 1.py
+++ Assignment 3 - Question 1.py
@@ -17,18 +17,22 @@
 
 import pandas
 import numpy
+import matplotlib.pyplot
 
-
+# get the file and column
 csvFileinput = pandas.read_csv("crime1.csv")
 columnOfViolentCrimesInPop = csvFileinput["ViolentCrimesPerPop"]
 
+
+# calculate the values needed
 meanOfViolentCrimesPerPop = numpy.mean(columnOfViolentCrimesInPop)
-medianOfViolentCrimesPerPop = numpy.mean(columnOfViolentCrimesInPop)
+medianOfViolentCrimesPerPop = numpy.median(columnOfViolentCrimesInPop)
 stdvOfViolentCrimesPerPop = numpy.std(columnOfViolentCrimesInPop)
 minOfViolentCrimesPerPop = numpy.min(columnOfViolentCrimesInPop)
 maxOfViolentCrimesPerPop = numpy.max(columnOfViolentCrimesInPop)
 
 
+# show output
 print("Mean: " + str(meanOfViolentCrimesPerPop))
 print("Median: " + str(medianOfViolentCrimesPerPop))
 print("Standard deviation : " + str(stdvOfViolentCrimesPerPop))
@@ -36,3 +40,19 @@ print("Minimum Value: " + str(minOfViolentCrimesPerPop))
 print("Maximum Value: " + str(maxOfViolentCrimesPerPop))
 
 
+
+
+
+# comparing the mean and median, it can be observed that the mean is larger then the median
+matplotlib.pyplot.hist(columnOfViolentCrimesInPop, bins = 25)
+matplotlib.pyplot.show()
+# along side with the histogram showing the distribution shows a slightly apparent right skew shape,
+# this is concluded by the "tail" shape thats going right and a "hill" found on the left
+
+
+
+# with outliers found at the value of 1.0, this causes the mean to deviate quite a bit
+# as seen from comparing it with the median, the mean is larger
+# this is explained by the way they are calculated, 
+# average/mean is calculated by the sum of all values and dividing it by the number of elements
+# while the median is the middle value of all the values
---
+++ Assignment 3 - Question 2.py
@@ -0,0 +1,17 @@
+### Question 2
+# use same dataset and col, create 2 plots
+# 
+#   histogram
+#   boxplot
+#   
+# both plots must include
+#   title
+#   X axis label
+#   Y axis label
+# 
+# Answer questions:
+#   what the hist shows about data values are spread
+#   box plot on the median
+#   does box plot present presence of outliers?
+#   
+### 
